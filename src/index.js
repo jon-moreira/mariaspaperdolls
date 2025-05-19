4f092a5512aaf90ae67a96613ca5d92ac51c86f3
expouic <jonasmoreira@gmail.com>
2025-05-19 23:30:41 +0100

    fix load styles

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Routing from "./routing";
 import * as serviceWorker from "./serviceWorker";
 import { I18nextProvider } from "react-i18next";
 
+import './App.css';
 import App from "./App";
 import i18n from "./i18n";
 
